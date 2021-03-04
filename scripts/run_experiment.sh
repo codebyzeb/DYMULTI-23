cc609bb042339ed5f07ae2ce23c5e868b1a3e067
zebulon <zg258@cam.ac.uk>
2021-03-04 09:49:16 +0000

    Implement script for quickly running experiments

---
+++ scripts/run_experiment.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Run basic experiment
+
+EXPERIMENT_DIR="experiments/experiment"
+TRANSCRIPT="/Users/zebulon/Documents/UniDocs/Year4/Project/Corpora/CHILDES_wordseg/phonemized/EnglishUK_Manchester_anne_10000utterances_phonemes.txt"
+CLEAN=FALSE
+SEGMENTER=${1:-baseline}
+
+if $CLEAN
+then
+    # Clearn directory
+    echo "Cleaning directory '$EXPERIMENT_DIR'"
+    rm -r $EXPERIMENT_DIR
+    mkdir $EXPERIMENT_DIR
+
+    # Prepare transcript for segmentation using wordseg
+    echo "Preparing transcript for segmentation in '$EXPERIMENT_DIR'"
+    wordseg-prep -u phone --gold $EXPERIMENT_DIR/gold.txt $TRANSCRIPT > $EXPERIMENT_DIR/prepared.txt
+fi
+
+# Run segmenter
+if (( $# < 1 ))
+then
+	echo "Running baseline segmenter with no additional arguments"
+elif (( $# < 2 ))
+then
+	echo "Running '$SEGMENTER' segmenter with no additional arguments"
+else
+	echo "Running '$SEGMENTER' segmenter with additional arguments '${@:2}'"
+fi
+python -m segmenter.$SEGMENTER -o $EXPERIMENT_DIR/segmented.txt ${@:2} $EXPERIMENT_DIR/prepared.txt
+
+# Evaluate segmentation using wordseg
+echo "Calculating statistics"
+wordseg-eval $EXPERIMENT_DIR/segmented.txt $EXPERIMENT_DIR/gold.txt > $EXPERIMENT_DIR/eval.txt
+less $EXPERIMENT_DIR/eval.txt | column -t
