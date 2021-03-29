36bb75919eefc759b1075ceff5bdfed27cea3103
zebulon <zg258@cam.ac.uk>
2021-03-29 15:32:33 +0100

    Implement lexicon probabilities

--- segmenter/lexicon.py
+++ segmenter/lexicon.py
@@ -16,6 +16,11 @@ class Lexicon(SortedDict):
         of frequency counts for each word.
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.type_count = self.__len__()
+        self.token_count = sum([self[k] for k in self])
+
     def increase_count(self, word, k=1):
         """ Increase the frequency count of a given word by k. `word` passed as a string. """
 
@@ -23,8 +28,10 @@ class Lexicon(SortedDict):
             return
         if not word in self:
             self.setdefault(word, k)
+            self.type_count += 1
         else:
             self[word] += k
+        self.token_count += k
 
     def __getitem__(self, key):
         """ Override default implementation to return 0 if the key is not in the dictionary """
@@ -32,6 +39,27 @@ class Lexicon(SortedDict):
         if not key in self:
             return 0
         return super().__getitem__(key)
+
+    def relative_freq(self, word, consider_unseen=False):
+        """ Returns the relative frequency of the word. consider_unseen smooths the probability
+        by adding the number of types to the denominator, accounting for unseen words """
+
+        if consider_unseen:
+            if self[word] == 0 or self.token_count + self.type_count == 0:
+                return 0
+            return self[word] / (self.token_count + self.type_count)
+        else:
+            if self[word] == 0 or self.token_count == 0:
+                return 0
+            return self[word] / self.token_count
+
+    def unseen_freq(self):
+        """ Returns the expected frequency of unseen words, such that the sum of all relative
+        frequencies for each seen word (using consider_unseen=True) plus this value is 1. """
+
+        if self.type_count == 0 or self.token_count == 0:
+            return 0
+        return self.type_count / (self.token_count + self.type_count)
 
 class LexiconFrequencyModel(PeakModel):
     """ A simple lexicon-based model for word segmentation.
--- segmenter/tests/test_lexicon.py
+++ segmenter/tests/test_lexicon.py
@@ -2,6 +2,32 @@
 
 from segmenter.lexicon import Lexicon
 
+"""
+----------------------------------------------
+            INITIALISATION TESTS
+----------------------------------------------
+"""
+
+def test_init_with_dict_sets_counts():
+
+    lexicon = Lexicon({"a" : 1, "b" : 3})
+
+    assert(lexicon.token_count == 4)
+    assert(lexicon.type_count == 2)
+
+def test_init_without_dict_sets_counts():
+
+    lexicon = Lexicon()
+
+    assert(lexicon.token_count == 0)
+    assert(lexicon.type_count == 0)
+
+"""
+----------------------------------------------
+            INCREASE COUNT TESTS
+----------------------------------------------
+"""
+
 def test_increase_count():
 
     lexicon = Lexicon()
@@ -14,12 +40,6 @@ def test_increase_count():
     assert(lexicon["other"] == 1)
     assert(list(lexicon) == ["other", "word"])
 
-def test_access_missing_word_gives_zero():
-
-    lexicon = Lexicon()
-
-    assert(lexicon["word"] == 0)
-
 def test_increase_count_invalid_word_does_nothing():
 
     lexicon = Lexicon()
@@ -28,3 +48,87 @@ def test_increase_count_invalid_word_does_nothing():
     lexicon.increase_count("", 10)
 
     assert(len(lexicon) == 0)
+
+def test_increase_count_increases_token_count():
+
+    lexicon = Lexicon()
+
+    lexicon.increase_count("word", 10)
+    lexicon.increase_count("thing", 1)
+
+    assert(lexicon.token_count == 11)
+
+def test_increase_count_increases_type_count():
+
+    lexicon = Lexicon()
+
+    lexicon.increase_count("word", 10)
+    lexicon.increase_count("thing", 1)
+
+    assert(lexicon.type_count == 2)
+
+"""
+----------------------------------------------
+            ACCESS TESTS
+----------------------------------------------
+"""
+
+def test_access_missing_word_gives_zero():
+
+    lexicon = Lexicon()
+
+    assert(lexicon["word"] == 0)
+
+"""
+----------------------------------------------
+            RELATIVE FREQUENCY TESTS
+----------------------------------------------
+"""
+
+def test_relative_frequency_seen():
+
+    lexicon = Lexicon({"word" : 3, "thing" : 1})
+
+    assert(lexicon.relative_freq("word") == 3/4)
+    assert(lexicon.relative_freq("thing") == 1/4)
+
+def test_relative_frequency_seen_consider_unseen():
+
+    lexicon = Lexicon({"word" : 3, "thing" : 1})
+
+    assert(lexicon.relative_freq("word", consider_unseen=True) == 3/6)
+    assert(lexicon.relative_freq("thing", consider_unseen=True) == 1/6)
+
+def test_relative_frequency_unseen():
+
+    lexicon = Lexicon({"word" : 3, "thing" : 1})
+
+    assert(lexicon.relative_freq("nope") == 0)
+
+def test_relative_frequency_empty():
+
+    lexicon = Lexicon()
+
+    assert(lexicon.relative_freq("thing") == 0)
+
+"""
+----------------------------------------------
+            UNSEEN FREQUENCY TESTS
+----------------------------------------------
+"""
+
+def test_unseen_frequency_seen():
+
+    lexicon = Lexicon({"word" : 3, "thing" : 1})
+
+    assert(lexicon.unseen_freq() == 2/6)
+
+def test_sum_of_unseen_and_seen_frequency_is_one():
+
+    lexicon = Lexicon({"word" : 3, "thing" : 1})
+
+    word_freq = lexicon.relative_freq("word", consider_unseen=True)
+    thing_freq = lexicon.relative_freq("thing", consider_unseen=True)
+    unseen_freq = lexicon.unseen_freq()
+
+    assert(word_freq + thing_freq + unseen_freq == 1)
